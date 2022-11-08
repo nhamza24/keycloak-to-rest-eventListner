000aee05182af491a17ba43e0918311658318fe9
Nabil Hamza <nabil.hamza@atos.net>
2022-11-08 10:19:08 +0100

    first commit

---
+++ src/main/java/com/nhamza/keycloaktoresteventListner/KeycloakToRestEventListnerApplication.java
@@ -0,0 +1,13 @@
+package com.nhamza.keycloaktoresteventListner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KeycloakToRestEventListnerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KeycloakToRestEventListnerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nhamza/keycloaktoresteventListner/KeycloakToRestEventListnerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nhamza.keycloaktoresteventListner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KeycloakToRestEventListnerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
